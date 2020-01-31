4f882112156102f44557c8db40c8398af007edaf
Jon Winsley <jon.winsley@gmail.com>
2020-01-30 21:36:14 -0500

    Adding webpack build and test html stage

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-console.log('hello world');
+import {Application} from 'pixi.js';
+
+const DEFAULTS = {
+    nbgFg: "000000",
+    nbgBg: "ffffff"
+}
+
+class NetworkBackground {
+    app: Application;
+    constructor(element: HTMLElement) {
+        this.app = new Application({
+            resizeTo: element
+        });
+        element.appendChild(this.app.view);
+
+        let parameters = Object.assign({}, DEFAULTS, element.dataset);
+        this.app.renderer.backgroundColor = parseInt(parameters.nbgBg, 16);
+    }
+
+    readParameters(element: HTMLElement) {
+        
+    }
+}
+
+let nbg = [];
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('[data-nbg-element]').forEach(element => {
+        if (!(element instanceof HTMLElement)) return;
+        nbg.push(new NetworkBackground(element));
+    });
+});
